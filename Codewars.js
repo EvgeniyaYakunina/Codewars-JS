96006b74c807fd23ee9f7e442d104b4a7f77474b
EvgeniyaYakunina <Yakoonina@gmail.com>
2023-03-09 12:02:54 +0300

    add task 8

--- Codewars.js
+++ Codewars.js
@@ -141,3 +141,24 @@ Examples
 
     return Math.ceil(year/100);
   }
+
+  /*Terminal Game - Create Hero Prototype
+ ******************************************************************************************
+In this first kata in the series, you need to define a Hero prototype to be used in a terminal game. The hero should have the following attributes:
+
+attribute	value
+name	    user argument or 'Hero'
+position	'00'
+health	    100
+damage	    5
+experience	0
+*/
+   // Решение:
+      function Hero (name = "Hero") {
+
+        this.name = name
+        this.position = "00"
+        this.health = 100
+        this.damage = 5
+        this.experience = 0
+    }
