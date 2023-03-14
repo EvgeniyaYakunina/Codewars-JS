6dad785a8acc25576f80e9c2dfa6b976e7e3d391
EvgeniyaYakunina <Yakoonina@gmail.com>
2023-03-14 09:40:47 +0300

    add task 13

--- Codewars.js
+++ Codewars.js
@@ -253,3 +253,16 @@ If one of the arguments is a number your code must coerce it into being a string
       
         return `${string1} ${string2}`
       }
+
+     /* Be Concise IV - Index of an element in an array
+*****************************************************************************************
+Provided is a function find which accepts two parameters
+ in the following order: array, element and returns
+ the index of the element if found and "Not found" otherwise. 
+ Your task is to shorten the code as much as possible in order
+  to meet the strict character count requirements of the Kata. 
+  (no more than 85) You may assume that all array elements are unique.
+  */
+     // Решение:
+        const find =(a,el) => {return a.indexOf(el)>=0 ? a.indexOf(el):"Not found";}
+
