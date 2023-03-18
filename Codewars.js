d05f77a20bdcd57f09251c1ee10855783b16448d
EvgeniyaYakunina <Yakoonina@gmail.com>
2023-03-18 13:14:23 +0300

    add task 28

--- Codewars.js
+++ Codewars.js
@@ -532,3 +532,12 @@ return true if they are in love and false if they aren't.
           return flower1 % 2 !== flower2 % 2;
         }
 // *****************************************************************************************
+
+   /* Super Duper Easy
+Make a function that returns the value multiplied 
+by 50 and increased by 6. If the value entered is 
+a string it should return "Error".
+*/
+   // Решение:
+        const problem = x =>typeof x === "number" ? x * 50 + 6:"Error"
+// *****************************************************************************************
