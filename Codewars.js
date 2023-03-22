88b10c1a8f2aa654387d175e750975533036b504
EvgeniyaYakunina <Yakoonina@gmail.com>
2023-03-22 16:24:52 +0300

    add tasks 33-36

--- Codewars.js
+++ Codewars.js
@@ -602,3 +602,76 @@ For example(Input --> Output):
          return Array.from(Array(n), (_,i)=>i+1)
          }
 // *****************************************************************************************
+
+   /* Grasshopper - If/else syntax debug
+If/else syntax debug
+While making a game, your partner, Greg, decided to create a function to check if the user 
+is still alive called checkAlive/CheckAlive/check_alive. Unfortunately, Greg made some errors
+ while creating the function.
+
+checkAlive/CheckAlive/check_alive should return true if the player's health is greater than
+ 0 or false if it is 0 or below.
+
+The function receives one parameter health which will always be a whole 
+number between -10 and 10.
+function checkAlive (health) {
+  if () {
+    health < 0
+    return false
+  } else () {
+    return true
+  }
+}
+*/
+   // Решение:
+         function checkAlive (health) {
+            return health > 0
+            }
+// *****************************************************************************************  
+
+   /* Grasshopper - Function syntax debugging
+A student was working on a function and made some
+syntax mistakes while coding. Help them find their 
+mistakes and fix them.
+*/
+   // Решение:
+         function main (verb, noun){
+            return verb + noun
+         }
+// ***************************************************************************************** 
+
+
+   /* Grasshopper - Variable Assignment Debug
+Fix the variables assigments so that this code 
+stores the string 'devLab' in the variable name.
+*/
+   // Решение:
+         var a = "dev";
+         var b = "Lab";
+
+         var name = a + b;
+// ***************************************************************************************** 
+
+   /* Grasshopper - Object syntax debug
+While making a zork-type game, you create an object of rooms. 
+Unfortunately, the game is not working. Find all of the errors in the 
+rooms object to get your game working again.
+*/
+   // Решение:
+         var rooms = {
+            first: {
+            description: 'This is the first room',
+            items: {
+               chair: 'The old chair looks comfortable',
+               lamp: 'This lamp looks ancient'
+            }
+         },
+            second: {
+            description: 'This is the second room',
+            items: {
+               couch: 'This couch looks like it would hurt your back',
+               table: 'On the table there is an unopened bottle of water'
+            }
+            },
+         }
+// ***************************************************************************************** 
