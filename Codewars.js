2eaf20647af0b4811ecf9490bde3f61165d8f895
EvgeniyaYakunina <Yakoonina@gmail.com>
2023-04-04 17:47:35 +0300

    add task 51

--- Codewars.js
+++ Codewars.js
@@ -947,3 +947,15 @@ Finally, return the padded string.
          }
 // *****************************************************************************************
 
+/* Beginner - Reduce but Grow
+Given a non-empty array of integers, return the result of 
+multiplying the values together in order. 
+Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+*/
+// Решение:
+function grow(x){
+   return x.reduce((a, b) => a * b);
+   }
+// *****************************************************************************************
