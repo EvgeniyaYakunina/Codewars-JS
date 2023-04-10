1fedcdc52376092ef786a405f7a4057ac8575bbb
EvgeniyaYakunina <Yakoonina@gmail.com>
2023-04-10 17:06:05 +0300

    add task 56

--- Codewars.js
+++ Codewars.js
@@ -1025,3 +1025,14 @@ As usual, your function/method should be pure, i.e. it should not mutate the ori
             return this.split("").map(a => a === a.toUpperCase() ? a.toLowerCase(): a.toUpperCase()).join("");
          }
 // *****************************************************************************************
+
+   /* Beginner Series #2 Clock
+Clock shows h hours, m minutes and s seconds after midnight.
+Your task is to write a function which returns the time
+since midnight in milliseconds.
+*/
+   // Решение:
+         function past(h, m, s){
+            return h * 3600000 + m * 60000 + s * 1000
+         }
+// ***************************************************************************************** 
