4ec3b431ede016fc51845776c4df779245671a7f
EvgeniyaYakunina <Yakoonina@gmail.com>
2023-06-04 19:28:24 +0300

    secret message

---
+++ [8]Jenny's secret message.js
@@ -0,0 +1,12 @@
+
+function greet(name){
+    if(name === "Johnny"){
+        return "Hello, my love!";
+    }else{
+        return "Hello, " + name + "!";
+    }
+}
+greet();
+
+
+// const greet = (name)=> name === "Johnny" ? "Hello, my love!" : "Hello, " + name + "!"
