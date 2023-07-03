7dc15ef088aaedf43d994dc0350485bc13116ef4
EvgeniyaYakunina <Yakoonina@gmail.com>
2023-07-03 09:07:15 +0300

    function that receive age

---
+++ [8]Drink about.js
@@ -0,0 +1,15 @@
+// Make a function that receive age, and return what they drink.
+
+function peopleWithAgeDrink(old) {
+    let age = old;
+    switch (true) {
+        case age <= 14:
+            return 'drink toddy';
+        case age <= 18:
+            return 'drink coke';
+        case age < 21:
+            return 'drink beer';
+        case age >= 21:
+            return 'drink whisky';
+    }
+}
